81fa94deefdee39370389707c86d497ef49635fa
Macas <35805288+MaciejM123@users.noreply.github.com>
2019-10-15 00:52:27 +0200

    Second Feature
    feature/cash_withdraw

---
+++ CashMachine/CashMachine/Services/CashWithdrawer.cs
@@ -0,0 +1,38 @@
+﻿using CashMachine.Data.DataBase;
+using CashMachine.Models.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CashMachine.Services
+{
+    public static class CashWithdrawer
+    {
+        public static bool WithdrawCash(Owner owner)
+        {
+            int cashToWithdraw;
+            Console.WriteLine("How much money do you want to withdraw?");
+            try
+            {
+                cashToWithdraw = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid data type.");
+                return false;
+            }
+
+            using (var context = new CardDbContext())
+            {
+                var card = context.Card
+                                   .Where(c => c.OwnerId == owner.Id)
+                                   .FirstOrDefault();
+                card.Money = card.Money - cashToWithdraw;
+                context.SaveChanges();
+
+                return true;
+            }
+        }
+    }
+}
